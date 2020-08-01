fdab7bcb5117819752c1a4968928f9584f26b14a
Beatriz Ferreira dos Santos <bfsantos@RDL724796.raiadrogasil.com>
2020-08-01 00:33:19 -0300

    task que imprime mensagem

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 const gulp = require('gulp');
 const htmin = require('gulp-htmlmin');
+const notify = require("gulp-notify");
 
-gulp.task('start', function(){
+ gulp.task('start', function(){
    return gulp.src(['./src/index.html'])
     .pipe(htmin({collapseWhitespace:true}))
     .pipe(gulp.dest('./build'));   
-});
+}); 
+
+gulp.task('mensagem', function() {
+    return gulp.src("./src/index.html")
+    .pipe(notify("Hello Gulp!"));
+})
